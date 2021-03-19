3b0bfdec031aebfc3e4431536dc706aad1954f96
Neeqstock <nicola.davanzo01@gmail.com>
2021-03-19 12:34:42 +0100

    MidNotesUtils

---
+++ Music/MidiNotesUtils.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NeeqDMIs.Music
+{
+    public static class MidiNotesUtils
+    {
+        public static List<MidiNotes> GetAllMidiNotesList()
+        {
+            return Enum.GetValues(typeof(MidiNotes)).Cast<MidiNotes>().ToList();
+        }
+    }
+}
